9c428862e990aa4fc7c9b5c6fc7da63e76fcc96c
mitchellscot <Mitchellscott@me.com>
2021-01-31 22:53:56 -0600

    configured pool to connect to databse on heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,26 @@
 const pg = require('pg');
+const url = require('url');
 
-const config = {
-  database: 'react_gallery', 
-  host: 'localhost', 
-  user: process.env.DB_USERNAME,     
-  password: process.env.DB_PASSWORD,    
-  port: 5432, 
-  max: 10, 
-  idleTimeoutMillis: 30000 
-};
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  };
+}
+else {
+  config = {
+    database: 'react_gallery', 
+    host: 'localhost', 
+    user: process.env.DB_USERNAME,     
+    password: process.env.DB_PASSWORD,    
+    port: 5432, 
+    max: 10, 
+    idleTimeoutMillis: 30000 
+  };
+}
+
 
 const pool = new pg.Pool(config);
 
@@ -18,6 +30,7 @@ pool.on("connect", () => {
 
 pool.on("error", (err) => {
   console.log("error connecting to postgres", err);
+  process.exit(-1);
 });
 
 module.exports = pool;
